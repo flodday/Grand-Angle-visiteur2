dbd18a976d1892e7c37d4a522b2f00e86d7b303d
Hanekeko (Bilal Benyagoub) <75778663+kanekiadd@users.noreply.github.com>
2024-03-21 10:19:35 +0100

    creation page contact + integration des variables root + integration des page politique et mentions

--- contact.php
+++ contact.php
@@ -1,16 +1,9 @@
 <?php
+ 
 require_once "config/pdo.php";
-
-$titre = "Contact";
-$nav = "Contact";
+$titre = "contact";
+$nav = "contact";
 include_once "includes/pages/nav.php";
-
-
-
-?>
-
-<div><h1>CONTACT</h1></div>
-
-<?php
+include_once "includes/components/rubrique_contact.php";
 include_once "includes/pages/footer.php";
 ?>
---
+++ includes/components/rubrique_contact.php
@@ -0,0 +1,13 @@
+<div class="container_contact">
+    <h2>Nous contacter</h2>
+    <br>
+    <h3>oû nous trouver?</h3>
+    <p> adresse: <br> 13 rue du Brade, 41000, Blois</p>
+    <img src="./assets/images/completed-google-places-label.png" alt="" class="google_map">
+    <div class="bg_contact_info">
+    <p>numéro de telephone : <br> 06060606</p>
+    <br>
+    <p>email: <br> <a href="#">DUBRAC@GrandAngle.com</a></p>
+    </div>
+    
+</div>
